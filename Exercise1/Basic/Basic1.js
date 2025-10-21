b279a6c773865c6aeba69b5e816c8f1133227211
ahemyu <hansovic7@gmail.com>
2025-10-21 17:36:24 +0200

    ex.1a): almost works

--- Exercise1/Basic/Basic1.js
+++ Exercise1/Basic/Basic1.js
@@ -10,10 +10,9 @@
 function drawPixelwiseCircle(canvas, center, radius, color) {
     let context = canvas.getContext("2d");
     let img = context.createImageData(200, 200);
-    
     //TODO 1.1a)      Copy the code from Example.js
     //                and modify it to create a 
-    //                circle.
+    //                circle.]
 
 	// "img.data" is a 1D array of size 200*200*4, containing the rgba pixel data in uint8 format.
 	// The image data layout is [height, width, channel], i.e. [200, 200, 4] in this case.
@@ -26,17 +25,41 @@ function drawPixelwiseCircle(canvas, center, radius, color) {
 
     // we store colors as vectors, you can access the elements like arrays (e.g. color[0]) or using .r/.g/.b or .x/.y/.z
     // all of these access the same value, as shown in the loop below.
-    console.log(color);
-    for (let i = 0; i < 4 * width * height; i += 4) {
-      img.data[i + 0] = color.r;	// access with r(ed), g(reen), b(lue) and a(lpha)
-      img.data[i + 1] = color.y;	// access with x, y, z and w
-      img.data[i + 2] = color[2]; // access like an array
-      img.data[i + 3] = 255;
-    }
+    // console.log(color);
 
+    //TODO: adust this to only fill pixels inside a circle with given center and radius
+    //Two ideas: Either for each pixel (which is represented as a continuous sequence of four ints (R, G, B, A)) we check if it is inside the circle
+    // using the formula: distance squared = (x - x_center) squared + (y - y_center) squared and if distance squared below or equal radius squared, it is inside circle
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < 4 * width * height; i += 4) {
+        // Calculate distance before incrementing coordinates
+        let distanceSquared = Math.pow((x-center[0]), 2) + Math.pow((y-center[1]), 2);
+
+        if(distanceSquared <= Math.pow(radius, 2)){
+            img.data[i + 0] = color.r;	// access with r(ed), g(reen), b(lue) and a(lpha)
+            img.data[i + 1] = color.y;	// access with x, y, z and w
+            img.data[i + 2] = color[2]; // access like an array
+            img.data[i + 3] = 255;
+        } else {
+            // Set background to white
+            img.data[i + 0] = 255;
+            img.data[i + 1] = 255;
+            img.data[i + 2] = 255;
+            img.data[i + 3] = 255;
+        }
+
+        // Increment coordinates AFTER using them
+        x++;
+        if (x == width){
+            y++;
+            x = 0;
+        }
+    }
+    
     context.putImageData(img, 0, 0);
 }
-
 
 /** __Draws a circle with contour on {canvas}__
  * 
