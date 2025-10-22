4fc110b48eba483a13b83e74d9750f3a44559122
ahemyu <hansovic7@gmail.com>
2025-10-22 17:20:54 +0200

    ex 1.3: works

--- Exercise1/Basic/Basic2.js
+++ Exercise1/Basic/Basic2.js
@@ -14,9 +14,30 @@
 function drawArcCircle(canvas, center_blank, center_contour, radius_blank, radius_contour_inner, width_contour, color_blank, color_contour_inner, color_contour_contour) {
     let context = canvas.getContext("2d", { willReadFrequently: true });
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     //TODO 1.2) Use the arc() and stroke() methods to rasterize the two circles from Task 1.1.
-
+    /** Task description: JavaScript provides us with some rendering utilities for drawing primitives, 
+     * some of which are the arc() method and the stroke() method. Have a look at the documentation 
+     * and implement the same circles as before using those methods. The Canvas lineWidth Property may help you with the Contour. 
+     * Instead of rendering into two separate canvases, draw the two circles from Task 1.1 into the same render area this time. 
+     * Use the given vec_to_string(color) function to convert the color vector to a CSS color value. Draw the circles clockwise! */
+    //Blank circle
+    //TODO: somwehow at the border there is still smth wrong (idk what)
+    context.beginPath();
+    context.arc(center_blank[0], center_blank[1], radius_blank, 0, 2* Math.PI);
+    context.fillStyle = vec_to_string(color_blank);
+    context.fill()
+    context.strokeStyle = "white";
+    context.stroke();
+    
+    //contour circle
+    context.beginPath();
+    context.arc(center_contour[0], center_contour[1], radius_contour_inner, 0, 2* Math.PI);
+    context.fillStyle = vec_to_string(color_contour_inner);
+    context.fill()
+    context.strokeStyle = vec_to_string(color_contour_contour);
+    context.lineWidth = width_contour;
+    context.stroke();
+    
 
     /** Converts a vector to a "rgb(r,g,b)" string used by javascript
      * @param {Vec} color - color to be converted
@@ -24,7 +45,4 @@ function drawArcCircle(canvas, center_blank, center_contour, radius_blank, radiu
     function vec_to_string(color) {
         return "rgb(" + color.r + "," + color.g + "," + color.b + ")"
     }
-
-
-
 }
