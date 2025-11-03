742c4c5d5bafc20f27c70ef8ac1cfb99426132b8
ahemyu <hansovic7@gmail.com>
2025-11-03 12:30:05 +0100

    ex2: task 2.1: finished

--- Exercise02/Basic/Basic1.js
+++ Exercise02/Basic/Basic1.js
@@ -64,33 +64,59 @@ function bresenham(image, line, pixelScale) {
     let y0 = Math.floor(line.startPoint.y);
     let x1 = Math.floor(line.endPoint.x);
     let y1 = Math.floor(line.endPoint.y);
-
-    // TODO 2.1     Write code to draw a line
+     // TODO 2.1     Write code to draw a line
     //              between the start point and
     //              the end point. To make things
     //              easier, there are some comments
     //              on what to do next: 
 
     // compute deltas and update directions
-
-
-
+    // absolute differences because we only care about magnitude
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    
+    // determine which direction we're moving in each axis
+    // if x1 < x0, we need to move in negative directionb (and vice versa for y0 and y1)
+    let direction_x = (x0 < x1) ? 1 : -1;
+    let direction_y = (y0 < y1) ? 1 : -1;
+    
+    // Initialize the error variable
+    // This tracks how far our pixel positions are from the ideal line
+    // If dx > dy, we're moving more horizontally, so we use dx - dy
+    // Otherwise, we're moving more vertically, so we negate it
+    let error = dx - dy;
+    
     // set initial coordinates
+    let x = x0;
+    let y = y0;
+    
 
-
-
-    // start loop to set nPixels 
-    let nPixels = 0; // think about how many pixels need to be set - zero is not correct ;)
-    for (let i = 0; i < nPixels; ++i) {
-        // set pixel using the helper function setPixelS()
-
-
-        // update error
-
-
-        // update coordinates depending on the error
-
-
+    // The maximum of dx and dy gives us the number of steps
+    let nPixels = Math.max(dx, dy) + 1;
+    
+     for (let i = 0; i < nPixels; i++) {
+        // Set the current pixel to black
+        setPixelS(image, new Point(x, y), line.color, pixelScale);
+        
+        // if we reached final position we are done
+        if (x === x1 && y === y1) {
+            break;
+        }
+        
+        // we take it times 2 to avoid floats 
+        let error_times_two = 2 * error;
+        
+        // Decide whether to step in the x direction
+        if (error_times_two > -dy) {
+            error -= dy;  // Adjust error for the x step
+            x += direction_x;     // Take a step in the x direction
+        }
+        
+        // Decide whether to step in the y direction
+        if (error_times_two < dx) {
+            error += dx;  // Adjust error for the y step
+            y += direction_y;     // Take a step in the y direction
+        }
     }
 }
 
