c8c0abf976d4f8e59b377e7e32aafde0b444583d
ahemyu <hansovic7@gmail.com>
2025-11-08 18:30:27 +0100

    ex3: Task 3.2b): use barycentric coordinates to only color edges of triangle

--- Exercise03/Basic/shader_wireframe.fs
+++ Exercise03/Basic/shader_wireframe.fs
@@ -24,7 +24,18 @@ void main(void)
 		//				and discard all pixels not considered 
 		//				edges (farther away from an edge than 
 		//				epsilon). Use the GLSL mechanism 'discard'.
+			
+		// if any of the components has a value smaller or equal to epsilon we keep it, otherwise we throw it away
 
+		if(color.r <= epsilon || color.g <= epsilon || color.b <= epsilon){
+			// TODO: render these
+			gl_FragColor = vec4(color, 1.0);
+
+		}else{
+			// TODO: These need to be discarded 
+
+			discard;
+		}
 
 	}
 }
