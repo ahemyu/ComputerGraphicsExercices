022e59a7b8f463f53dd7721d02bddedbf0d1f8d9
ahemyu <hansovic7@gmail.com>
2025-11-23 12:50:04 +0100

    advanced5: init

---
+++ AdvancedFramework/build/src/Advanced05/CMakeFiles/Advanced05.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ahemyu/Desktop/ComputerGraphicsExercices/AdvancedFramework/src/Advanced05/src/cg.cpp" "src/Advanced05/CMakeFiles/Advanced05.dir/src/cg.cpp.o" "gcc" "src/Advanced05/CMakeFiles/Advanced05.dir/src/cg.cpp.o.d"
+  "/home/ahemyu/Desktop/ComputerGraphicsExercices/AdvancedFramework/src/Advanced05/src/main.cpp" "src/Advanced05/CMakeFiles/Advanced05.dir/src/main.cpp.o" "gcc" "src/Advanced05/CMakeFiles/Advanced05.dir/src/main.cpp.o.d"
+  "" "/home/ahemyu/Desktop/ComputerGraphicsExercices/AdvancedFramework/src/Advanced05/Advanced05" "gcc" "src/Advanced05/CMakeFiles/Advanced05.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AdvancedFramework/build/src/Advanced05/CMakeFiles/Advanced05.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Advanced05.dir/link.d"
+  "/home/ahemyu/Desktop/ComputerGraphicsExercices/AdvancedFramework/src/Advanced05/Advanced05"
+  "/home/ahemyu/Desktop/ComputerGraphicsExercices/AdvancedFramework/src/Advanced05/Advanced05.pdb"
+  "CMakeFiles/Advanced05.dir/src/cg.cpp.o"
+  "CMakeFiles/Advanced05.dir/src/cg.cpp.o.d"
+  "CMakeFiles/Advanced05.dir/src/main.cpp.o"
+  "CMakeFiles/Advanced05.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advanced05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
