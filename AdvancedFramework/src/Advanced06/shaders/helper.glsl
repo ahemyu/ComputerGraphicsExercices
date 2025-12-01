56bf77ba43f571f7b2e3b943d7987b96b0120aaf
ahemyu <hansovic7@gmail.com>
2025-12-01 18:13:06 +0100

    adv6: 1a): spherical coords for texture mapping

--- AdvancedFramework/src/Advanced06/shaders/helper.glsl
+++ AdvancedFramework/src/Advanced06/shaders/helper.glsl
@@ -4,8 +4,14 @@ vec2 cartesianToSpherical(vec3 n)
     // Convert cartesian coordinates to spherical coordinates. 
     // For computing the inverse tangent, use the two-argument
     // version of atan().
-    float theta = 0;
-    float phi = 0;
+    // theta = 1/tan(z/x)
+    // phi = 1/cos(y)
+    float x = n[0];
+    float y = n[1];
+    float z = n[2];
+    float theta = atan(z, x);
+    float phi = acos(y);
+
     return vec2(theta,phi);
 }
 
@@ -16,7 +22,15 @@ vec3 sphericalToCartesian(vec2 a)
 
     // TODO 6.4 a)
     // Convert spherical coordinates to cartesian coordinates.
-    return vec3(0,0,0);
+    // x = sin(phi) * cos(theta)
+    // y  = cos(phi)
+    // z = sin(phi) * sin(theta)
+
+    float x = sin(phi) * cos(theta);
+    float y = cos(phi);
+    float z = sin(phi) * sin(theta);
+
+    return vec3(x,y,z);
 }
 
 
@@ -30,6 +44,12 @@ vec2 sphericalToTexture(vec2 a)
     // Compute texture coordinates from spherical coordinates.
 	// Do not forget to mirror both coordinates to have the north pole at the top 
 	// and France located west of Germany! ;)
-    return vec2(0,0);
+  // ok  so u correspoinds to theta and v to phi 
+  // problem is that theta is in [-pi, pi] but u needs to be in [0, 1]
+  // same for phi, it is in [0, pi] but v needs to be in [0, 1]
+ 
+    float u = 1 - ((theta + PI) / (2*PI)); //we do 1 - to mirror it
+    float v = 1 - (phi / PI); //same here
+    return vec2(u,v);
 }
 
